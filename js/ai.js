ccf4a289654d0d632534d1c659aa39bca34f6f2f
R. David Dunphy <rdd.dunphy@gmail.com>
2018-04-24 17:17:35 +0100

    Let AI know about walls

--- js/ai.js
+++ js/ai.js
@@ -1,25 +1,35 @@
 function AI(game) {
     this.game = game;
-    this.ww = game.settings.ww;
-    this.wh = game.settings.wh;
 }
 
 AI.prototype.getMove = function() {
+    this.ww = game.settings.ww;
+    this.wh = game.settings.wh;
+    this.wrap = game.settings.wrap;
     var head = this.game.snake.head;
     var fruit = this.game.fruit.cell;
     var fruitMove;
-    if (head.x != fruit.x) {
-        if (head.x < fruit.x && Math.abs(head.x - fruit.x) > this.ww / 2
-        || head.x > fruit.x && Math.abs(head.x - fruit.x) < this.ww / 2) {
-            fruitMove = Dir.LEFT;
+
+    if (!this.wrap) {
+        if (head.x != fruit.x) {
+            fruitMove = (head.x > fruit.x) ? Dir.LEFT : Dir.RIGHT;
         } else {
-            fruitMove = Dir.RIGHT;
+            fruitMove = (head.y > fruit.y) ? Dir.UP : Dir.DOWN;
         }
-    } else if (head.y < fruit.y && Math.abs(head.y - fruit.y) > this.wh / 2
-        || head.y > fruit.y && Math.abs(head.y - fruit.y) < this.wh / 2) {
-        fruitMove = Dir.UP;
     } else {
-        fruitMove = Dir.DOWN;
+        if (head.x != fruit.x) {
+            if (head.x < fruit.x && Math.abs(head.x - fruit.x) > this.ww / 2
+            || head.x > fruit.x && Math.abs(head.x - fruit.x) < this.ww / 2) {
+                fruitMove = Dir.LEFT;
+            } else {
+                fruitMove = Dir.RIGHT;
+            }
+        } else if (head.y < fruit.y && Math.abs(head.y - fruit.y) > this.wh / 2
+            || head.y > fruit.y && Math.abs(head.y - fruit.y) < this.wh / 2) {
+            fruitMove = Dir.UP;
+        } else {
+            fruitMove = Dir.DOWN;
+        }
     }
     var preferredMoves = [
         fruitMove,
@@ -40,6 +50,6 @@ AI.prototype.isTerminalMove = function(move) {
     var snakeCopy = this.game.snake.copy();
     snakeCopy.changeDir(move);
     var newHead = snakeCopy.next(this.ww, this.wh);
-    console.log(newHead);
-    return snakeCopy.isDead(newHead, true);
+    console.log(this.wrap);
+    return snakeCopy.isDead(newHead, this.wrap);
 };
